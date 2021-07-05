0e0da2a473ff8237e8b3607146c9f1dbe37abbcd
imrshu <rishabh.verma11998@gmail.com>
2021-07-05 21:28:35 +0530

    add env vars

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
 
-
 function App() {
   return (
     <BrowserRouter>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,16 @@ import 'firebase/auth'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBlKquEeS7V4hb1kV9rYd2Gtr5JhLkLu-s",
-    authDomain: "jragrau-868e6.firebaseapp.com",
-    projectId: "jragrau-868e6",
-    storageBucket: "jragrau-868e6.appspot.com",
-    messagingSenderId: "536044627817",
-    appId: "1:536044627817:web:38bc1997504a0675123ff8"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.firestore().settings({timestampsInSnapshots: true});
+
 export default firebase;
