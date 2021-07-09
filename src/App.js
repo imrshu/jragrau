726bb1736c6e2b18fb8b4a41187f08e0cebf395f
imrshu <rishabh.verma11998@gmail.com>
2021-07-09 19:14:16 +0530

    Add: components structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <h1>Hello world!</h1>
+        <Header />
+        <Footer />
       </div>
     </BrowserRouter>
   )
