0094e75556ef67cfe136adb1e24d5ca23544cd99
imrshu <rishabh.verma11998@gmail.com>
2021-07-11 13:13:16 +0530

    Refactor the code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
-import Footer from './components/Footer'
+// import Footer from './components/Footer'
 
 
 function App() {
@@ -9,7 +9,6 @@ function App() {
     <BrowserRouter>
       <div className="boxed">
         <Header />
-        <Footer />
       </div>
     </BrowserRouter>
   )
