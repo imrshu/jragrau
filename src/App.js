9d41346d4151986db929dd32eab80bc64b263177
garima-25 <garimasachdeva25@gmail.com>
2021-07-14 00:57:13 +0530

    about and navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import About_us from './components/About_us'
+import Navbar from './components/Navbar'
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <div className="boxed">
         <Header />
         <Footer />
+        <Navbar />
+        <About_us />
       </div>
     </BrowserRouter>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,61 @@ class Navbar extends Component {
     render() {
         return (
             <div>
-                
+                <header id="header" className="header header-classic clearfix">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                            <div className="header-wrap clearfix">
+                                <div id="logo" className="logo"><Link to="index.html" rel="home"> <img src={logo} alt="Jragrau Industries Brand Logo" width="150"/> </Link></div>
+                                <div className="nav-wrap">
+                                    <div className="btn-menu"> </div>
+                                    <nav id="mainnav" className="mainnav">
+                                        <ul className="menu">
+                                        <li className="home">
+                                            <Link to="index.html">Home</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="about.html">About</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="portfolio-v1.html">Products</Link>
+                                            <ul className="submenu">
+                                                <li><Link to="portfolio-v2.html">Load Cells</Link></li>
+                                                <li><Link to="portfolio-v3.html">Torque Sensors</Link></li>
+                                                <li><Link to="portfolio-v4.html">Calibration Systems</Link></li>
+                                                <li><Link to="portfolio-single-v1.html">Testing Systems</Link></li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <Link to="services.html">Custom Machinery</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="blog-fullwidth.html">Calibration</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="contact.html">Catalogues</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="contact.html">Software</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="contact.html">Contact</Link>
+                                        </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                                <div className="sidebar">
+                                    <div className="widget widget-search top-search">
+                                        <form id="searchform" action="index-header-classic.html#" method="get">
+                                        <div><input id="s" className="sss" type="text" placeholder="Type &amp; Hit Enter..." /> <input id="searchsubmit" type="submit" value="" /></div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
             </div>
         )
     }
