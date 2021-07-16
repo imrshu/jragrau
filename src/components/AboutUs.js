70efad234777d51d3c3c0f32644b4ee6b2a0e1d8
imrshu <rishabh.verma11998@gmail.com>
2021-07-16 13:10:45 +0530

    refactor the components

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,151 +6,152 @@ class AboutUs extends Component {
     render() {
         return (
             <div>
-               <div className="page-title parallax parallax3">
-            <div className="overlay"> </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="page-title-heading">
-                        <h2 className="title">Get in touch with us</h2>
-                        </div>
-                        <div className="breadcrumbs">
-                        <ul>
-                            <li className="home">
-                                <Link to="/">Home</Link></li>
-                            <li>Contact</li>
-                        </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-            <section className="flat-row">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="title-section style1 large">
-                        <h3 className="title">We Making a Positive Impact Across The Globe</h3>
-                        <div className="icon"><img src="images/icon/line.png" alt="image" /></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="flat-icon">
-                        <div className="col-md-4">
-                        <div className="iconbox center circle">
-                            <div className="box-header"> </div>
-                            <div className="icon-post">
-                                <div className="box-title">
-                                    <Link to="/About_us">Quality Work</Link></div>
-                                <div className="box-content">
-                                    <p>We ensure that all projects are done with utmost professionalism using quality materials while offering clients the support and accessibility.</p>
+                <div className="page-title parallax parallax3">
+                    <div className="overlay"> </div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="page-title-heading">
+                                <h2 className="title">Get in touch with us</h2>
+                                </div>
+                                <div className="breadcrumbs">
+                                <ul>
+                                    <li className="home">
+                                        <Link to="/">Home</Link></li>
+                                    <li>Contact</li>
+                                </ul>
                                 </div>
                             </div>
                         </div>
-                        </div>
-                        <div className="col-md-4">
-                        <div className="iconbox center circle">
-                            <div className="box-header"> </div>
-                            <div className="icon-post">
-                                <div className="box-title"><Link to="about.html#">Leadership</Link></div>
-                                <div className="box-content">
-                                    <p>We has a distinguished history of high achievement. Our strategy supports the best researchers, provides an exceptional student experience and assists our international partners.</p>
-                                </div>
-                            </div>
-                        </div>
-                        </div>
-                        <div className="col-md-4">
-                        <div className="iconbox center circle">
-                            <div className="box-header"> </div>
-                            <div className="icon-post">
-                                <div className="box-title"><a href="about.html#">Priorities</a></div>
-                                <div className="box-content">
-                                    <p>Deadlines aren’t bad. They help you organize your time. They help you set priorities. They make you get going when you might not feel like it.</p>
-                                </div>
-                            </div>
-                        </div>
-                        </div>
                     </div>
                 </div>
-            </div>
-            </section>
-            <section className="flat-row parallax bg-section-about">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h3 className="text-center text-white font-family-heading font-size-30 margin-bottom-20">WE BUILD PROFESSIONAL AND SMART BUILDINGS</h3>
-                        <div className="text-center margin-top-30"><a className="flat-button button-style" href="about.html#">GET A QOUTE</a></div>
-                    </div>
-                </div>
-            </div>
-            </section> 
 
-            <section id="services" className="flat-row what-we-do offer">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                    <div className="title-section style1 large">
-                        <h3 className="title">OUR TEAM</h3>
-                        <div className="icon"><img src="images/icon/line.png" alt="image" /></div>
-                        <div className="desc">We’re tightly connected with our communities.</div>
-                    </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-3 col-xs-12">
-                    <div className="flat-team style5">
-                        <div className="avatar">
-                            <div className="overlay"> </div>
-                            <img src="images/member/3.jpg" alt="image" />
+                <section className="flat-row">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="title-section style1 large">
+                                <h3 className="title">We Making a Positive Impact Across The Globe</h3>
+                                <div className="icon"><img src="images/icon/line.png" alt="image" /></div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="content">
-                            <h3 className="name">Dedicated Teams</h3>
-                            <p className="position">Makerting</p>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-md-3 col-xs-12">
-                    <div className="flat-team style5">
-                        <div className="avatar">
-                            <div className="overlay"> </div>
-                            <img src="images/member/3.jpg" alt="image" />
-                        </div>
-                        <div className="content">
-                            <h3 className="name">True Partners</h3>
-                            <p className="position">Founder &amp; CEO</p>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-md-3 col-xs-12">
-                    <div className="flat-team style5">
-                        <div className="avatar">
-                            <div className="overlay"> </div>
-                            <img src="images/member/4.jpg" alt="image" />
-                        </div>
-                        <div className="content">
-                            <h3 className="name">Global Know-How</h3>
-                            <p className="position">Web Design</p>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-md-3 col-xs-12">
-                    <div className="flat-team style5">
-                        <div className="avatar">
-                            <div className="overlay"> </div>
-                            <img src="images/member/5.jpg" alt="image" />
-                        </div>
-                        <div className="content">
-                            <h3 className="name">Focus on Innovation</h3>
-                            <p className="position">Web Design</p>
+                        <div className="row">
+                            <div className="flat-icon">
+                                <div className="col-md-4">
+                                <div className="iconbox center circle">
+                                    <div className="box-header"> </div>
+                                    <div className="icon-post">
+                                        <div className="box-title">
+                                            <Link to="/About_us">Quality Work</Link></div>
+                                        <div className="box-content">
+                                            <p>We ensure that all projects are done with utmost professionalism using quality materials while offering clients the support and accessibility.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                                <div className="col-md-4">
+                                <div className="iconbox center circle">
+                                    <div className="box-header"> </div>
+                                    <div className="icon-post">
+                                        <div className="box-title"><Link to="about.html#">Leadership</Link></div>
+                                        <div className="box-content">
+                                            <p>We has a distinguished history of high achievement. Our strategy supports the best researchers, provides an exceptional student experience and assists our international partners.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                                <div className="col-md-4">
+                                <div className="iconbox center circle">
+                                    <div className="box-header"> </div>
+                                    <div className="icon-post">
+                                        <div className="box-title"><a href="about.html#">Priorities</a></div>
+                                        <div className="box-content">
+                                            <p>Deadlines aren’t bad. They help you organize your time. They help you set priorities. They make you get going when you might not feel like it.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                </section>
+
+                <section className="flat-row parallax bg-section-about">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h3 className="text-center text-white font-family-heading font-size-30 margin-bottom-20">WE BUILD PROFESSIONAL AND SMART BUILDINGS</h3>
+                                <div className="text-center margin-top-30"><a className="flat-button button-style" href="about.html#">GET A QOUTE</a></div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            </section>
-            </div>
-            
+                </section> 
+
+                <section id="services" className="flat-row what-we-do offer">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                            <div className="title-section style1 large">
+                                <h3 className="title">OUR TEAM</h3>
+                                <div className="icon"><img src="images/icon/line.png" alt="image" /></div>
+                                <div className="desc">We’re tightly connected with our communities.</div>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-3 col-xs-12">
+                            <div className="flat-team style5">
+                                <div className="avatar">
+                                    <div className="overlay"> </div>
+                                    <img src="images/member/3.jpg" alt="image" />
+                                </div>
+                                <div className="content">
+                                    <h3 className="name">Dedicated Teams</h3>
+                                    <p className="position">Makerting</p>
+                                </div>
+                            </div>
+                            </div>
+                            <div className="col-md-3 col-xs-12">
+                            <div className="flat-team style5">
+                                <div className="avatar">
+                                    <div className="overlay"> </div>
+                                    <img src="images/member/3.jpg" alt="image" />
+                                </div>
+                                <div className="content">
+                                    <h3 className="name">True Partners</h3>
+                                    <p className="position">Founder &amp; CEO</p>
+                                </div>
+                            </div>
+                            </div>
+                            <div className="col-md-3 col-xs-12">
+                            <div className="flat-team style5">
+                                <div className="avatar">
+                                    <div className="overlay"> </div>
+                                    <img src="images/member/4.jpg" alt="image" />
+                                </div>
+                                <div className="content">
+                                    <h3 className="name">Global Know-How</h3>
+                                    <p className="position">Web Design</p>
+                                </div>
+                            </div>
+                            </div>
+                            <div className="col-md-3 col-xs-12">
+                            <div className="flat-team style5">
+                                <div className="avatar">
+                                    <div className="overlay"> </div>
+                                    <img src="images/member/5.jpg" alt="image" />
+                                </div>
+                                <div className="content">
+                                    <h3 className="name">Focus on Innovation</h3>
+                                    <p className="position">Web Design</p>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>   
         )
     }
 }
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -7,66 +7,66 @@ class ContactUs extends Component {
         return (
             <div>
                <div className="page-title parallax parallax5">
-                <div className="overlay"> </div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                        <div className="page-title-heading">
-                            <h2 className="title">Get in touch with us</h2>
-                        </div>
-                        <div className="breadcrumbs">
-                            <ul>
-                                <li className="home"><Link to="contact.html#">Home</Link></li>
-                                <li>Contact</li>
-                            </ul>
-                        </div>
+                    <div className="overlay"> </div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                            <div className="page-title-heading">
+                                <h2 className="title">Get in touch with us</h2>
+                            </div>
+                            <div className="breadcrumbs">
+                                <ul>
+                                    <li className="home"><Link to="contact.html#">Home</Link></li>
+                                    <li>Contact</li>
+                                </ul>
+                            </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                </div>
-                <p> </p>
+
                 <section className="main-content">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                        <div className="flat-contact">
-                            <div className="post-contact">
-                                <h4 className="title">Contact Us</h4>
-                                <p>Get in touch with the professionals for quality services.</p>
-                            </div>
-                            <div className="contact-form">
-                                <form id="contactform" className="comment-form" action="http://themesflat.com/html/monster/contact/contact-process.php" method="post" novalidate="">
-                                    <fieldset className="style-1 full-name"><input id="name" className="tb-my-input" tabindex="1" name="name" size="32" type="text" value="" placeholder="Your Name *" /></fieldset>
-                                    <fieldset className="style-1 email-address"><input id="email" className="tb-my-input" tabindex="2" name="email" size="32" type="text" value="" placeholder="Your Email *" /></fieldset>
-                                    <fieldset className="style-1 phone"><input id="phone" className="tb-my-input" tabindex="1" name="phone" size="32" type="text" value="" placeholder="Your Phone Number *" /></fieldset>
-                                    <fieldset className="style-1 subject"><input id="subject" className="tb-my-input" tabindex="2" name="subject" size="32" type="text" value="" placeholder="Company" /></fieldset>
-                                    <fieldset className="message-form"><textarea id="comment-message" tabindex="4" name="message" rows="8" placeholder="Message"></textarea></fieldset>
-                                    <div className="submit-wrap"><button className="flat-button button-style style-v1">Submit</button></div>
-                                </form>
-                            </div>
-                        </div>
-                        </div>
-                        <div className="col-md-6">
-                        <div className="flat-contact-us">
-                            <div className="contact-us">
-                                <div className="contact-address">
-                                    <h4 className="title v1">Address</h4>
-                                    <p>MIE Industrial area, Bahadurgarh, IND</p>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6">
+                            <div className="flat-contact">
+                                <div className="post-contact">
+                                    <h4 className="title">Contact Us</h4>
+                                    <p>Get in touch with the professionals for quality services.</p>
+                                </div>
+                                <div className="contact-form">
+                                    <form id="contactform" className="comment-form" action="http://themesflat.com/html/monster/contact/contact-process.php" method="post" novalidate="">
+                                        <fieldset className="style-1 full-name"><input id="name" className="tb-my-input" tabindex="1" name="name" size="32" type="text" value="" placeholder="Your Name *" /></fieldset>
+                                        <fieldset className="style-1 email-address"><input id="email" className="tb-my-input" tabindex="2" name="email" size="32" type="text" value="" placeholder="Your Email *" /></fieldset>
+                                        <fieldset className="style-1 phone"><input id="phone" className="tb-my-input" tabindex="1" name="phone" size="32" type="text" value="" placeholder="Your Phone Number *" /></fieldset>
+                                        <fieldset className="style-1 subject"><input id="subject" className="tb-my-input" tabindex="2" name="subject" size="32" type="text" value="" placeholder="Company" /></fieldset>
+                                        <fieldset className="message-form"><textarea id="comment-message" tabindex="4" name="message" rows="8" placeholder="Message"></textarea></fieldset>
+                                        <div className="submit-wrap"><button className="flat-button button-style style-v1">Submit</button></div>
+                                    </form>
                                 </div>
                             </div>
-                            <div className="contact-us v1">
-                                <div className="contact-info">
-                                    <h4 className="title v2">Contact Info</h4>
-                                    <p>Phone:<Link to="contact.html#"> +91-9811490150</Link></p>
-                                    <p>Mail:<Link to="contact.html#"> support@jragrau.com</Link></p>
-                                </div>
                             </div>
-                            <div className="container-fluid"> </div>
-                        </div>
+                            <div className="col-md-6">
+                            <div className="flat-contact-us">
+                                <div className="contact-us">
+                                    <div className="contact-address">
+                                        <h4 className="title v1">Address</h4>
+                                        <p>MIE Industrial area, Bahadurgarh, IND</p>
+                                    </div>
+                                </div>
+                                <div className="contact-us v1">
+                                    <div className="contact-info">
+                                        <h4 className="title v2">Contact Info</h4>
+                                        <p>Phone:<Link to="contact.html#"> +91-9811490150</Link></p>
+                                        <p>Mail:<Link to="contact.html#"> support@jragrau.com</Link></p>
+                                    </div>
+                                </div>
+                                <div className="container-fluid"> </div>
+                            </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                </section> 
+                </section>
             </div>
         )
     }
